89cd6b9ddf448cd2b48aabfa5e3008a3576491e2
pa-ershov <pa-ershov@yandex.ru>
2020-03-16 07:08:17 +0300

    недоделано

--- les_1.py
+++ les_1.py
@@ -1,10 +1,15 @@
 import requests
+import json
+import time
 
-url = 'https://5ka.ru/special_offers/'
-headers = ('User-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36')
-response = requests.get(url, headers=headers)
+START_URL = 'https://5ka.ru/api/v2/special_offers/'
+CAT_URL = 'https://5ka.ru/api/v2/categories/'
+HEADERS = {'User-Agent':
+               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
 
 
+def x5ka():
 
 
-
+if __name__ == '__main__':
+    x5ka()
