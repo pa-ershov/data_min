daf92b7c36c2b69f357c142717810de597aaab54
pa-ershov <pa-ershov@yandex.ru>
2020-03-16 06:54:18 +0300

    11

--- les_1.py
+++ les_1.py
@@ -1,6 +1,10 @@
+import requests
+
+url = 'https://5ka.ru/special_offers/'
+headers = ('User-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36')
+response = requests.get(url, headers=headers)
 
 
 
 
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36
 
